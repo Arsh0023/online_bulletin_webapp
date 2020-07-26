92c8a0204bc69d66684778de0d0a74e06e38de12
Arshpreet Singh <arsharsh2391@gmail.com>
2020-07-27 01:06:54 +0530

    added students model with basic fields

--- messenger_app/models.py
+++ messenger_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class students(models.Model):
+    name = models.CharField(max_lenght=264)
+    phone_no = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    stream = models.CharField(max_lenght="10")
+    year = models.IntegerField()
+
+    def __str__(self):
+        return("{}, stream - {} ,year-{} ".format(self.name,self.stream,self.year))
